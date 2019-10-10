749c9d5fbec3d2921b877e3923e35cae33a726d0
Wyatt Pfeil <wyatt.pfeil@gmail.com>
2019-10-09 20:32:31 -0400

    Added Buttons, Refined Rectangle

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ class Vector2 {
   }
 }
 
-class NewRect {
+class Rectangle {
   constructor(Width, Height) {
     this.tworect = two.makeRectangle(
       window.innerWidth / 2,
@@ -22,6 +22,11 @@ class NewRect {
       window.innerWidth * Width,
       window.innerHeight * Height
     );
+    this.position = Vector2.new(0.5, 0.5);
+    this.size = Vector2.new(0.25, 0.25);
+    this.innerColor = Color3.fromRGB(255, 0, 0);
+    this.outline = 0;
+    this.opacity = 1;
   }
   set position(NewPos) {
     var NewX = NewPos.x;
@@ -32,6 +37,20 @@ class NewRect {
     );
     this.setPropertyAndUpdate("position", NewPos);
   }
+  set outline(NewOutline) {
+    this.tworect.linewidth = NewOutline;
+    this.setPropertyAndUpdate("outline", NewOutline);
+  }
+  get outline() {
+    return this._outline;
+  }
+  set opacity(NewOpacity) {
+    this.tworect.opacity = NewOpacity;
+    this.setPropertyAndUpdate("opacity", NewOpacity);
+  }
+  get opacity() {
+    return this._opacity;
+  }
   get position() {
     return this._position;
   }
@@ -40,11 +59,23 @@ class NewRect {
     var g = NewColor.green;
     var b = NewColor.blue;
     console.log(r, g, b);
-    this.tworect.fill = "rgba(" + r + "," + g + "," + b + ", 0.75)";
+    this.tworect.fill = "rgba(" + r + "," + g + "," + b + ", 1)";
     this.setPropertyAndUpdate("innerColor", NewColor);
   }
   get innerColor() {
     return this._innerColor;
+  }
+  set size(NewSize) {
+    console.log("New size is ");
+    console.log(NewSize);
+    var SizeX = window.innerWidth * NewSize.x;
+    var SizeY = window.innerHeight * NewSize.y;
+    this.tworect.width = SizeX;
+    this.tworect.height = SizeY;
+    this.setPropertyAndUpdate("size", NewSize);
+  }
+  get size() {
+    return this._size;
   }
   setPropertyAndUpdate(PropName, PropValue) {
     this["_" + PropName] = PropValue;
@@ -52,9 +83,83 @@ class NewRect {
   }
 }
 
-var nrect = new NewRect(0.5, 0.4);
-nrect.position = Vector2.new(0.2, 0, 2);
-nrect.innerColor = Color3.fromRGB(255, 0, 0);
+class Button {
+  constructor(Width, Height) {
+    this.rectangle = new Rectangle(0.25, 0.25);
+    this.backtangle = new Rectangle(0.25, 0.25);
+    this.backtangle.position = Vector2.new(
+      this.rectangle.position.x,
+      this.rectangle.position.y + 0.025
+    );
+    this.backtangle.opacity = 0.5;
 
-console.log(nrect.innerColor);
-console.log(nrect.position);
+    document.addEventListener("click", this.click.bind(this));
+
+    document.addEventListener("mousedown", this.mousedown.bind(this));
+
+    document.addEventListener("mouseup", this.mouseup.bind(this));
+  }
+
+  click(e) {
+    if (this.isPointInRectangle(e.clientX, e.clientY, this.rectangle)) {
+      console.log("Clicked!");
+    }
+  }
+  mouseup(e) {
+    if (this.isPointInRectangle(e.clientX, e.clientY, this.rectangle)) {
+      console.log("mouse up in square!");
+      this.rectangle.position = Vector2.new(
+        this.backtangle.position.x,
+        this.backtangle.position.y - 0.025
+      );
+    }
+  }
+  mousedown(e) {
+    if (this.isPointInRectangle(e.clientX, e.clientY, this.rectangle)) {
+      console.log("Mouse Down in Rectangle!");
+      this.rectangle.position = this.backtangle.position;
+    }
+  }
+  isPointInRectangle(X, Y, Rectangle) {
+    var CenterOfButtonX = Rectangle.position.x * window.innerWidth;
+    var CenterOfButtonY = Rectangle.position.y * window.innerHeight;
+    var SizeOfButtonX = Rectangle.size.x * window.innerWidth;
+    var SizeOfButtonY = Rectangle.size.y * window.innerHeight;
+
+    var LeftSideOfButton = CenterOfButtonX - SizeOfButtonX / 2;
+    var RightSideOfButton = CenterOfButtonX + SizeOfButtonX / 2;
+
+    var TopOfButton = CenterOfButtonY - SizeOfButtonY / 2;
+    var BottomOfButton = CenterOfButtonY + SizeOfButtonY / 2;
+
+    if (
+      X > LeftSideOfButton &&
+      X < RightSideOfButton &&
+      Y > TopOfButton &&
+      Y < BottomOfButton
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  set position(NewPos) {
+    var NewX = NewPos.x;
+    var NewY = NewPos.y;
+    this.rectangle.position = Vector2.new(NewPos.x, NewPos.y + 0.025)
+    this.backtangle.position = NewPos
+    this.setPropertyAndUpdate("position", NewPos);
+  }
+
+}
+//var nrect = new Rectangle(0.25,0.25);
+var rectangle = new Rectangle(0.1, 0.1);
+rectangle.position = Vector2.new(0.2, 0.2);
+rectangle.innerColor = Color3.fromRGB(0, 255, 0);
+var button = new Button(0.5, 0.5);
+//var button2 = new Button(0.2, 0.2);
+//button2.position = Vector2.new(0.9, 0.9);
+
+//console.log(nrect.innerColor);
+//console.log(nrect.position);
